9fde17e30b9806818c481570049e58e3eb58bf79
Théo Monnom <theo.monnom@outlook.com>
2023-05-09 12:39:41 +0200

    add comments

--- examples/simple_room/main.cpp
+++ examples/simple_room/main.cpp
@@ -7,6 +7,11 @@ int main(int argc, char *argv[])
     Room room{};
     room.Connect("ws://localhost:7880", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjQ5MjM1OTQ4MjMsImlzcyI6IkFQSVRzRWZpZFpqclFvWSIsIm5hbWUiOiJ3ZWIiLCJuYmYiOjE2ODM1OTQ4MjMsInN1YiI6IndlYiIsInZpZGVvIjp7InJvb20iOiJsaXZla2l0LWZmaS10ZXN0Iiwicm9vbUpvaW4iOnRydWV9fQ.voLT9RK3wNYEGdWovPLv1BzyN1v5tpJ59e0DIqIVfiU");
 
+
+    // Should we implement a mechanism to PollEvents/WaitEvents? Like SDL2/glfw
+    //   - So we can remove the useless loop here
+    // Or is it better to use callback based events?
+
     while(true) {
 
     }
