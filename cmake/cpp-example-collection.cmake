cc4992f6758baeeb14b6a98dfd18b815adc9b138
Stephen DeRosa <stephen.derosa@livekit.io>
2026-04-02 11:57:42 -0600

    compiling

--- cmake/cpp-example-collection.cmake
+++ cmake/cpp-example-collection.cmake
@@ -5,16 +5,23 @@
 include_guard(GLOBAL)
 
 function(livekit_configure_cpp_example_collection)
-  set(LIVEKIT_CPP_EXAMPLES_SOURCE_DIR
-      "${CMAKE_SOURCE_DIR}/cpp-example-collection")
-  set(LIVEKIT_CPP_EXAMPLES_INSTALL_PREFIX
-      "${CMAKE_BINARY_DIR}/../local-install"
-      CACHE PATH "Install prefix used by cpp-example-collection")
-  set(LIVEKIT_CPP_EXAMPLES_BINARY_DIR
-      "${CMAKE_BINARY_DIR}/cpp-example-collection-build"
-      CACHE PATH "Build directory for cpp-example-collection")
-  set(LIVEKIT_CPP_EXAMPLES_LIVEKIT_DIR
-      "${LIVEKIT_CPP_EXAMPLES_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/cmake/LiveKit")
+  # Absolute paths so out-of-tree builds and symlinks behave consistently.
+  get_filename_component(LIVEKIT_CPP_EXAMPLES_SOURCE_DIR
+    "${CMAKE_SOURCE_DIR}/cpp-example-collection" ABSOLUTE)
+
+  get_filename_component(_lk_examples_install_default
+    "${CMAKE_SOURCE_DIR}/local-install" ABSOLUTE)
+  set(LIVEKIT_CPP_EXAMPLES_INSTALL_PREFIX "${_lk_examples_install_default}"
+    CACHE PATH "Install prefix used by cpp-example-collection")
+
+  get_filename_component(_lk_examples_binary_default
+    "${CMAKE_BINARY_DIR}/cpp-example-collection-build" ABSOLUTE)
+  set(LIVEKIT_CPP_EXAMPLES_BINARY_DIR "${_lk_examples_binary_default}"
+    CACHE PATH "Build directory for cpp-example-collection")
+
+  get_filename_component(LIVEKIT_CPP_EXAMPLES_LIVEKIT_DIR
+    "${LIVEKIT_CPP_EXAMPLES_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/cmake/LiveKit"
+    ABSOLUTE)
 
   if(NOT EXISTS "${LIVEKIT_CPP_EXAMPLES_SOURCE_DIR}/CMakeLists.txt")
     message(FATAL_ERROR
@@ -33,12 +40,18 @@ function(livekit_configure_cpp_example_collection)
     VERBATIM
   )
 
+  # cmake --install /Users/sderosa/workspaces/client-sdk-cpp/build-debug --prefix ~/livekit-sdk-local
+
+
   add_custom_target(cpp_example_collection ALL
+    COMMAND ${CMAKE_COMMAND} --install "${CMAKE_BINARY_DIR}"
+            --prefix "${LIVEKIT_CPP_EXAMPLES_INSTALL_PREFIX}"
     COMMAND ${CMAKE_COMMAND} -S "${LIVEKIT_CPP_EXAMPLES_SOURCE_DIR}"
             -B "${LIVEKIT_CPP_EXAMPLES_BINARY_DIR}"
-            -DCMAKE_PREFIX_PATH="${LIVEKIT_CPP_EXAMPLES_INSTALL_PREFIX}"
-            -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"
-            -DLiveKit_DIR="${LIVEKIT_CPP_EXAMPLES_LIVEKIT_DIR}"
+            "-DCMAKE_PREFIX_PATH=${LIVEKIT_CPP_EXAMPLES_INSTALL_PREFIX}"
+            "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+            "-DLIVEKIT_LOCAL_SDK_DIR=${LIVEKIT_CPP_EXAMPLES_INSTALL_PREFIX}"
+            "-DLiveKit_DIR=${LIVEKIT_CPP_EXAMPLES_LIVEKIT_DIR}"
     COMMAND ${CMAKE_COMMAND} --build "${LIVEKIT_CPP_EXAMPLES_BINARY_DIR}"
             --config "$<CONFIG>"
     DEPENDS install_livekit_sdk_for_examples
