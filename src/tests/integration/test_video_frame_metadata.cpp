e8e4b299180267229c86f0137516a14a387b2fd2
Stephen DeRosa <stephen.derosa@livekit.io>
2026-04-23 10:11:32 -0600

    unit and integration breakup

--- src/tests/integration/test_video_frame_metadata.cpp
+++ src/tests/integration/test_video_frame_metadata.cpp
@@ -14,92 +14,15 @@
  * limitations under the License.
  */
 
-#include "room_proto_converter.h"
 #include "tests/common/test_common.h"
 #include "video_utils.h"
 
-#include <algorithm>
 #include <chrono>
 #include <condition_variable>
 #include <mutex>
 #include <optional>
 
 namespace livekit::test {
-
-TEST(VideoFrameMetadataTest, EmptyMetadataIsOmittedFromProto) {
-  std::optional<VideoFrameMetadata> metadata = VideoFrameMetadata{};
-  EXPECT_FALSE(toProto(metadata).has_value());
-}
-
-TEST(VideoFrameMetadataTest, UserTimestampOnlyIsPreserved) {
-  VideoFrameMetadata metadata;
-  metadata.user_timestamp = 123;
-
-  auto proto_metadata = toProto(metadata);
-  ASSERT_TRUE(proto_metadata.has_value());
-  EXPECT_TRUE(proto_metadata->has_user_timestamp());
-  EXPECT_EQ(proto_metadata->user_timestamp(), 123u);
-  EXPECT_FALSE(proto_metadata->has_frame_id());
-
-  auto round_trip = fromProto(*proto_metadata);
-  ASSERT_TRUE(round_trip.has_value());
-  EXPECT_EQ(round_trip->user_timestamp, std::optional<std::uint64_t>(123));
-  EXPECT_EQ(round_trip->frame_id, std::nullopt);
-}
-
-TEST(VideoFrameMetadataTest, FrameIdOnlyIsPreserved) {
-  VideoFrameMetadata metadata;
-  metadata.frame_id = 456;
-
-  auto proto_metadata = toProto(metadata);
-  ASSERT_TRUE(proto_metadata.has_value());
-  EXPECT_FALSE(proto_metadata->has_user_timestamp());
-  EXPECT_TRUE(proto_metadata->has_frame_id());
-  EXPECT_EQ(proto_metadata->frame_id(), 456u);
-
-  auto round_trip = fromProto(*proto_metadata);
-  ASSERT_TRUE(round_trip.has_value());
-  EXPECT_EQ(round_trip->user_timestamp, std::nullopt);
-  EXPECT_EQ(round_trip->frame_id, std::optional<std::uint32_t>(456));
-}
-
-TEST(VideoFrameMetadataTest, BothFieldsArePreserved) {
-  VideoFrameMetadata metadata;
-  metadata.user_timestamp = 123;
-  metadata.frame_id = 456;
-
-  auto proto_metadata = toProto(metadata);
-  ASSERT_TRUE(proto_metadata.has_value());
-  EXPECT_TRUE(proto_metadata->has_user_timestamp());
-  EXPECT_TRUE(proto_metadata->has_frame_id());
-
-  auto round_trip = fromProto(*proto_metadata);
-  ASSERT_TRUE(round_trip.has_value());
-  EXPECT_EQ(round_trip->user_timestamp, std::optional<std::uint64_t>(123));
-  EXPECT_EQ(round_trip->frame_id, std::optional<std::uint32_t>(456));
-}
-
-TEST(VideoFrameMetadataTest, EmptyProtoMetadataIsIgnored) {
-  proto::FrameMetadata metadata;
-  EXPECT_FALSE(fromProto(metadata).has_value());
-}
-
-TEST(TrackPublishOptionsTest, PacketTrailerFeaturesRoundTrip) {
-  TrackPublishOptions options;
-  options.packet_trailer_features.user_timestamp = true;
-  options.packet_trailer_features.frame_id = true;
-
-  proto::TrackPublishOptions proto_options = toProto(options);
-  ASSERT_EQ(proto_options.packet_trailer_features_size(), 2);
-  EXPECT_EQ(proto_options.packet_trailer_features(0),
-            proto::PacketTrailerFeature::PTF_USER_TIMESTAMP);
-  EXPECT_EQ(proto_options.packet_trailer_features(1),
-            proto::PacketTrailerFeature::PTF_FRAME_ID);
-
-  TrackPublishOptions round_trip = fromProto(proto_options);
-  EXPECT_TRUE(round_trip.packet_trailer_features.user_timestamp);
-  EXPECT_TRUE(round_trip.packet_trailer_features.frame_id);
-}
 
 class VideoFrameMetadataServerTest : public LiveKitTestBase {};
 
